9e9c9ce07b53a50979603b75922b1dd88788e3f2
Josh Nussbaum <joshnuss@gmail.com>
2011-10-14 05:48:52 -0400

    added html template for order cancelation email

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe OrderMailer do
-  let(:order) { Factory(:order) }
+  let(:order) { Factory(:order, 
+                        :completed_at => Time.now, 
+                        :bill_address => Factory(:address), 
+                        :ship_address => Factory(:address)) }
 
   context "confirmation email" do
     let(:email) { OrderMailer.confirm_email(order) }
+
+    specify { email.content_type.should match("text/html") }
+  end
+
+  context "cancelation email" do
+    let(:email) { OrderMailer.cancel_email(order) }
 
     specify { email.content_type.should match("text/html") }
   end
