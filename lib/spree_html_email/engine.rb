7cb69bbcd20ba976e31ef151c0f6c57957f6598a
Michael Bianco <iloveitaly@gmail.com>
2012-03-26 18:00:42 -0400

    Added support for spree_digital's multi-attachment on variants; improving formatting
    * List multiple links for a single variant that has multiple digital files
    * Improving formatting / UX

--- lib/spree_html_email/engine.rb
+++ lib/spree_html_email/engine.rb
@@ -4,17 +4,8 @@ module SpreeHtmlEmail
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    # use rspec for tests
-    config.generators do |g|
-      g.test_framework :rspec
-    end
-
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-        Rails.application.config.cache_classes ? require(c) : load(c)
-      end
-
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
