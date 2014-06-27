ddbe6ac91ecc80463ecb529a3caba384696e2a6d
Michael Bianco <iloveitaly@gmail.com>
2014-06-27 10:16:55 -0400

    Adding back base/helper to mailer views to fix broken specs
    https://github.com/spree/spree/commit/1a69dff2b817d0bc059eabbdbec83f373bb506e6#commitcomment-6823223

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,3 +1,5 @@
 Spree::OrderMailer.class_eval do
+  helper 'spree/base'
+  
   layout 'spree/layouts/email'
 end
--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -1,3 +1,4 @@
 Spree::ShipmentMailer.class_eval do
   layout 'spree/layouts/email'
+  helper 'spree/base'
 end
